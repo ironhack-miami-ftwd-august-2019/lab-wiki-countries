a7508d47adf7bd3b28f1fc24069eab03ca58e75b
Tzikas <niko.tzikas@gmail.com>
2019-10-03 15:27:27 -0400

    Updates

---
+++ precache-manifest.2a3915cc30131b7d29a235f540097580.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9511adb88d85d685d940",
+    "url": "/lab-wiki-countries/static/css/main.7c5cc214.chunk.css"
+  },
+  {
+    "revision": "9511adb88d85d685d940",
+    "url": "/lab-wiki-countries/static/js/main.9511adb8.chunk.js"
+  },
+  {
+    "revision": "51d55e968b858b704d39",
+    "url": "/lab-wiki-countries/static/css/1.e7f6339b.chunk.css"
+  },
+  {
+    "revision": "51d55e968b858b704d39",
+    "url": "/lab-wiki-countries/static/js/1.51d55e96.chunk.js"
+  },
+  {
+    "revision": "e09c17c1a3eabe4a13b8",
+    "url": "/lab-wiki-countries/static/js/runtime~main.e09c17c1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lab-wiki-countries/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f93f48fa138c41513d5fbce2a7910678",
+    "url": "/lab-wiki-countries/index.html"
+  }
+];
